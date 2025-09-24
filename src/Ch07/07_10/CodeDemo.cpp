599e3bc8a4fbf45858b8eb7ab924d3e24610c6ca
Zachary James <140269823+Yuplox@users.noreply.github.com>
2025-09-24 20:34:25 +0000

    Completed Chapter 7

--- src/Ch07/07_10/CodeDemo.cpp
+++ src/Ch07/07_10/CodeDemo.cpp
@@ -9,7 +9,37 @@
 #include <utility>
 
 class Person{
-    // Write your code here
+private:
+    std::string name;
+    float energy;
+    float happiness;
+    float health;
+
+public:
+    Person(const std::string& name, float energy, float happiness, float health) {
+        this->name = name;
+        this->energy = energy;
+        this->happiness = happiness;
+        this->health = health;
+    }
+
+    void Eat(float calories) {
+        energy += (calories * 7.0 / 200.0);
+    }
+
+    void Play(float minutes) {
+        happiness += minutes / 2.0;
+        energy -= minutes / 3.0;
+    }
+
+    void Sleep(float hours) {
+        energy += hours * 3.75;
+        health += hours * 2.5;
+    }
+
+    float GetEnergy() const {return energy;}
+    float GetHappiness() const {return happiness;}
+    float GetHealth() const {return health;}
 };
 
 int main(){
